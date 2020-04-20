121127899d045ea290678ff6c1d840555b13f312
505799690 <37524434+505799690@users.noreply.github.com>
2020-04-20 09:45:22 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,6 +8,9 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
+git clone https://github.com/fw876/helloworld package/luci-app-ssr-plus
+./scripts/feeds update -a && ./scripts/feeds install -a
+
 rm -f ./.config*
 touch ./.config
 
